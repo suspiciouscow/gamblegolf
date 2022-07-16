b4dce787e283379dda14ada92f01f287490d6130
kirbt <91921906+kirbt@users.noreply.github.com>
2022-07-16 13:45:22 -0700

    Changes to ball drag mechanic

--- Gamble Golf/Assets/Scripts/BallScript.cs
+++ Gamble Golf/Assets/Scripts/BallScript.cs
@@ -10,27 +10,35 @@ public class BallScript : MonoBehaviour
     public Vector3 minPower;
     public Vector3 maxPower;
 
-    Camera cam;
+    Camera camera;
     Vector3 force;
     Vector3 startPoint;
     Vector3 endPoint;
 
 
     void Start() {
-        cam = Camera.main;
+        camera = Camera.main;
     }
 
 
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
+            // Get Mouse Position when press
             Vector3 mousePos = Input.mousePosition;
             mousePos.z = 10;
-            startPoint = Camera.main.Screen[ToWorldPoint(mousePos);
+            startPoint = camera.ScreenToWorldPoint(mousePos);
             Debug.Log(startPoint);
-        if (Input.GetMouseButton)
-            force = new Vector3(-Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x)
-                                , 0
-                                , -Mathf.Clamp(startPoint.z - endPoint.z, minPower.z, maxPower.z));
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            // Get Mouse Position when let go
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 10;
+            endPoint = camera.ScreenToWorldPoint(mousePos);
+
+            // Shoot ball
+            force = new Vector3(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x)
+                                , 0         
+                                , Mathf.Clamp(startPoint.z - endPoint.z, minPower.z, maxPower.z));
             rb.AddForce(force * power, ForceMode.Impulse);
         }
 
