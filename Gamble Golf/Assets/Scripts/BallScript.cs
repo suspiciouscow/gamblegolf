e95a507841338b24e2ddb53a2dd3f1ef2c2c46ac
kirbt <91921906+kirbt@users.noreply.github.com>
2022-07-16 16:39:55 -0700

    pushing p

--- Gamble Golf/Assets/Scripts/BallScript.cs
+++ Gamble Golf/Assets/Scripts/BallScript.cs
@@ -40,7 +40,10 @@ public class BallScript : MonoBehaviour {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
