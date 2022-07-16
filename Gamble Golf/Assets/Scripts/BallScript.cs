3e98b0d3ed025b923045218c444f5f18c2c2f9b9
kirbt <91921906+kirbt@users.noreply.github.com>
2022-07-16 16:45:27 -0700

    stay mad sebastian

--- Gamble Golf/Assets/Scripts/BallScript.cs
+++ Gamble Golf/Assets/Scripts/BallScript.cs
@@ -34,32 +34,10 @@ public class BallScript : MonoBehaviour {
         if (Input.GetMouseButtonDown(0)) {
             Vector3 mousePos = Input.mousePosition;
             mousePos.z = 10;
-            startPoint = Camera.main.Screen[ToWorldPoint(mousePos);
+            startPoint = Camera.main.ScreenToWorldPoint(mousePos);
             Debug.Log(startPoint);
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-        if (Input.GetMouseButton)
-            force = new Vector3(-Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x)
-                                , 0
-                                , -Mathf.Clamp(startPoint.z - endPoint.z, minPower.z, maxPower.z));
-            rb.AddForce(force * power, ForceMode.Impulse);
 
         }
-
         // Render line FX during button hold
         if (Input.GetMouseButton(0)) {
             Vector3 mousePos = Input.mousePosition;
