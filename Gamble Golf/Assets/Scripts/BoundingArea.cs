3b24c3440eba0e7ac7ba917d28b1e8f3b0e20c01
mikisaf <s-safronovm@bsd405.org>
2022-07-16 17:50:09 -0700

    OutOfBounds
    out of bounds added to scene and script

---
+++ Gamble Golf/Assets/Scripts/BoundingArea.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundingArea : MonoBehaviour
+{
+    public Vector3 SpawnPoint;
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("BoundObj"))
+        {
+            other.gameObject.transform.position = SpawnPoint;
+            other.attachedRigidbody.velocity = Vector3.zero;
+            other.attachedRigidbody.angularVelocity = Vector3.zero;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
