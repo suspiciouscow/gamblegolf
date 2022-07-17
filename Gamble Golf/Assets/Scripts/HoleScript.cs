c2a1632924dd2e7b0752db1e32e38aceede06fa0
Sebastian <77251765+galaxygoldfish@users.noreply.github.com>
2022-07-16 20:37:02 -0700

    switch to next scene

--- Gamble Golf/Assets/Scripts/HoleScript.cs
+++ Gamble Golf/Assets/Scripts/HoleScript.cs
@@ -1,23 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HoleScript : MonoBehaviour
 {
     
     public ParticleSystem particleSystem;
+    public string nextLevel;
 
     void Start() {
     }
-
-
-
 
     void OnTriggerEnter(Collider collider) 
     {
         if(collider.gameObject.tag == "Ball") {
             collider.gameObject.SetActive(false);
             particleSystem.Play();
+            SceneManager.LoadScene(nextLevel);
         }
     }
+
 }
